7ff222cfa1aa3a1ebfa7c02df237ab424add8406
LiangBoqing <Boqing.Liang@hearbest.jp>
2023-06-07 16:48:31 +0900

    08.encoding_error_demo.py

---
+++ 08.encoding_error_demo.py
@@ -0,0 +1,36 @@
+import platform
+
+print("Python version: ", platform.python_version())
+print("Version tuple: ", platform.python_version_tuple())
+print("Compiler: ", platform.python_compiler())
+print("Build: ", platform.python_build())
+
+print("\nSystem information: ")
+print("System: ", platform.system())
+print("Node: ", platform.node())
+print("Release: ", platform.release())
+print("Version: ", platform.version())
+print("Machine: ", platform.machine())
+print("Processor: ", platform.processor())
+
+
+text = "リューチェーンを構築することが、農家の所得向上につながり、最終的には農業現場の待遇向上にもつながる。生産者が、コンビニエンスストアに商品を納めるベンダーや外食チェーン向けに契約栽培をしたり、商品開発に参画したりといった事例が増えている。そうしてフードバリューチェーンを構築することが、農家の所得向上につながり、最終的には農業現場の待遇向上にもつながる。"
+
+
+r""" 実行結果：
+D:\GitHome\hb_pyhton_study\venv\Scripts\python.exe D:\GitHome\hb_pyhton_study\08.encoding_error_demo.py 
+Python version:  3.8.10
+Version tuple:  ('3', '8', '10')
+Compiler:  MSC v.1928 64 bit (AMD64)
+Build:  ('tags/v3.8.10:3d8993a', 'May  3 2021 11:48:03')
+
+System information: 
+System:  Windows
+Node:  SUAGELENOVO
+Release:  10
+Version:  10.0.19045
+Machine:  AMD64
+Processor:  Intel64 Family 6 Model 140 Stepping 1, GenuineIntel
+
+プロセスは終了コード 0 で終了しました
+"""
