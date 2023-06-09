b408227c63183f3798d391f500c574d0987d7e0e
LiangBoqing <Boqing.Liang@hearbest.jp>
2023-06-09 15:37:56 +0900

    21.import.py

---
+++ 21.import.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import math
+print(math.pi)  # 3.141592653589793
+print(math.cos(0))  # 1.0
+
+import math as m
+print(m.pi)  # 3.141592653589793
+print(m.cos(0))  # 1.0
+
+from math import pi, cos
+print(pi)  # 3.141592653589793
+print(cos(0))  # 1.0
+
+from math import pi as p
+from math import cos as c
+print(p)  # 3.141592653589793
+print(c(0))  # 1.0
