fecbdc599ac63fdc80600ee64feedaa98efc82bf
LiangBoqing <Boqing.Liang@hearbest.jp>
2023-06-09 15:36:53 +0900

    10.list_1.py

---
+++ 10.list_1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+l1 = []
+l2 = [1, 2]
+l3 = list()
+l4 = list('abcd')
+l5 = list(('ab', 'cd'))
+l6 = [[1, 2], [3, 4]]
+l7 = [[1, 2, 0.5], ['Hello', [5, object()]]]
+
+print(l1)  # []
+print(l2)  # [1, 2]
+print(l3)  # []
+print(l4)  # ['a', 'b', 'c', 'd']
+print(l5)  # ['ab', 'cd']
+print(l6)  # [[1, 2], [3, 4]]
+print(l7)  # [[1, 2, 0.5], ['Hello', [5, <object object at 0x00000153F3814160>]]]
+
